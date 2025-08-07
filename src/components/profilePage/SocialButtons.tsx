e2e84c74db4e594bf1012933f156cc13aff78239
VoHoangKiet <muoivo.051064@gmail.com>
2025-08-07 21:41:15 +0700

    rm box

--- src/components/profilePage/SocialButtons.tsx
+++ src/components/profilePage/SocialButtons.tsx
@@ -1,7 +1,6 @@
 import { Space } from "antd";
 import { FaThreads, FaInstagram, FaFacebookF } from "react-icons/fa6";
 import SocialButton from "./SocialButton";
-import GiftBox from "./GiftBox";
 import { delay } from "../../utils/delay";
 
 interface SocialLink {
@@ -52,7 +51,7 @@ export default function SocialButtons({ socialLinks = defaultSocialLinks }: Soci
 
   return (
     <Space className="!mt-6 flex w-full" direction="vertical">
-      <GiftBox />
+      {/* <GiftBox /> */}
       {socialLinks.map((link) => (
         <SocialButton
           key={link.platform}
